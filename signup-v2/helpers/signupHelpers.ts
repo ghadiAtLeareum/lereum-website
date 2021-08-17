17beeba08f306e833aa13df317f76af8dbe47c85
ghadiAtLeareum <85280321+ghadiAtLeareum@users.noreply.github.com>
2021-08-17 08:49:19 +0300

    Item still not working

--- signup-v2/helpers/signupHelpers.ts
+++ signup-v2/helpers/signupHelpers.ts
@@ -1,12 +1,14 @@
+import apiResponses from "../helpers/apiResponses";
 import * as AWS from "aws-sdk";
-import * as _dynamodbAutoMarshaller from "@aws/dynamodb-auto-marshaller";
 import * as axios from "axios";
 import { environment } from "../environment";
-import { create } from "domain";
 const dynamoDb = new AWS.DynamoDB();
 const documentClient = new AWS.DynamoDB.DocumentClient();
-const marshaller = new _dynamodbAutoMarshaller.Marshaller();
 const cognitoidentityserviceprovider = new AWS.CognitoIdentityServiceProvider();
+import { Marshaller } from "@aws/dynamodb-auto-marshaller";
+const marshaller = new Marshaller({ unwrapNumbers: true });
+const SES = new AWS.SES();
+import { v4 as uuidv4 } from "uuid";
 
 export class SignupHelpers {
   verificationTable: string;
@@ -21,6 +23,7 @@ export class SignupHelpers {
   email: string;
   lastName: string;
   firstName: string;
+  item: {};
 
   constructor() {
     this.verificationTable = environment.VERIFICATION_DYNAMODB_TABLE;
@@ -30,15 +33,7 @@ export class SignupHelpers {
     this.verifyRecaptcha(this.recaptchaToken);
     this.checkEmailExisted(this.email);
     this.checkIfUserExistsInDynamo(this.email);
-    this.upsertSignupUserToDynamoDb(
-      this.id,
-      this.email,
-      this.token,
-      this.firstName,
-      this.lastName,
-      this.createdAt,
-      this.expiresAt
-    );
+    this.upsertSignupUserToDynamoDb(this.item);
   }
 
   async verifyRecaptcha(recaptchaToken: string) {
@@ -47,12 +42,12 @@ export class SignupHelpers {
       response: recaptchaToken,
       remoteip: undefined,
     };
-    const verifyResponse = await axios.default({
+    await axios.default({
       method: "post",
       url: "https://www.google.com/recaptcha/api/siteverify",
       params: payload,
     });
-    return verifyResponse.data.success;
+    return true;
   }
 
   checkEmailExisted = async (email: string) => {
@@ -80,7 +75,6 @@ export class SignupHelpers {
       },
       ScanIndexForward: true,
     };
-
     const retrievedUser = await documentClient.query(params).promise();
     if (retrievedUser.Items.length > 0) {
       return true;
@@ -89,60 +83,57 @@ export class SignupHelpers {
     }
   };
 
-  upsertSignupUserToDynamoDb = async (
-    id: string,
-    email: string,
-    token: string,
-    firstName: string,
-    lastName: string,
-    expiresAt: number,
-    createdAt: number
-  ) => {
+  getUsernameInfo = async (email: string) => {
     const params = {
-      "TableName": "verification",
-      Item: {
-        "recipient": {
-          S: email,
-        },
-        "type": {
-          S: "SIGN_UP_EMAIL",
-        },
-        "id": {
-          S: id,
-        },
-        "payload": {
-          M: {
-            "userId": {
-              S: id,
-            },
-            "firstName": {
-              S: firstName,
-            },
-            "lastName": {
-              S: lastName,
-            },
-            "email": {
-              S: email,
-            },
-            "createdAt": {
-              N: createdAt,
-            },
+      TableName: "verification",
+      KeyConditionExpression: "#DYNOBASE_recipient = :pkey",
+      ExpressionAttributeValues: {
+        ":pkey": email,
+      },
+      ExpressionAttributeNames: {
+        "#DYNOBASE_recipient": "recipient",
+      },
+      ScanIndexForward: true,
+    };
+    const retrievedUser = await documentClient.query(params).promise();
+    return retrievedUser;
+  };
+
+  upsertSignupUserToDynamoDb = async (item: Record<string, any>) => {
+    console.log("item is");
+    console.log(item);
+    const params = {
+      TableName: "verification",
+      Item: marshaller.marshallItem(item),
+    };
+    console.log("params are");
+    console.log(params);
+    await dynamoDb.putItem(params).promise();
+    return true;
+  };
+
+  async sendTokenEMAIL(email: string, firstName: string, email_token: string) {
+    let base_url = "bitsouks123.com";
+    let link_url = base_url + "/" + email + "/" + firstName + "/" + email_token;
+    var params = {
+      Destination: {
+        ToAddresses: [email],
+      },
+      Message: {
+        Body: {
+          Text: {
+            Charset: "UTF-8",
+            Data: link_url,
           },
         },
-        "token": {
-          S: token,
-        },
-        "created_at": {
-          N: createdAt,
-        },
-        "expiresAt": {
-          N: expiresAt,
+        Subject: {
+          Charset: "UTF-8",
+          Data: "Bitsouks Verification Link",
         },
       },
+      Source: "ghadi@lereum.com",
     };
-    console.log(params)
-    console.log("Adding a new item...");
-    const result = await documentClient.put(params).promise()
-    if (result) return result
-  };
+    await SES.sendEmail(params).promise();
+    return true;
+  }
 }
--- signup-v2/lambdas/signupMainLambda.ts
+++ signup-v2/lambdas/signupMainLambda.ts
@@ -4,77 +4,76 @@ import { SignupHelpers } from "../helpers/signupHelpers";
 const signupHelpers = new SignupHelpers();
 import "source-map-support/register";
 import { v4 as uuidv4 } from "uuid";
-import { Console } from "console";
-import { Lambda } from "aws-sdk";
 
 export const handler: APIGatewayProxyHandler = async (event) => {
   const body = JSON.parse(event.body);
   const email = body.email;
   const recaptchaToken = body.recaptchaToken;
   const agreement = body.agreement;
-  
+  const firstName = body.firstName;
+  const lastName = body.lastName;
+  console.log(event);
 
-  if (!recaptchaToken) {
-    return apiResponses._400({ message: "missing text fom the body" });
-  }
-  if (!agreement) {
-    return apiResponses._400({ message: "missing agreement from the body" });
-  }
+  // try {
+  //   if (!recaptchaToken) {
+  //     return apiResponses._400({ message: "missing text fom the body" });
+  //   }
+  //   if (!agreement) {
+  //     return apiResponses._400({ message: "missing agreement from the body" });
+  //   }
 
-  if (!email) {
-    return apiResponses._400({ message: "missing email from the body" });
-  }
+  //   if (!email) {
+  //     return apiResponses._400({ message: "missing email from the body" });
+  //   }
 
-  var recaptchaResult = await signupHelpers.verifyRecaptcha(
-    body.recaptchaToken
-  );
-  console.log(recaptchaResult);
-  if (!recaptchaResult) {
-    console.log(recaptchaResult);
-  }
+  //   var recaptchaResult = await signupHelpers.verifyRecaptcha(recaptchaToken);
+  //   console.log("here is the recaptcha check");
+  //   if (!recaptchaResult) {
+  //     return apiResponses._400({ message: "Recaptcha not verified" });
+  //   } else {
+  //     console.log("recaptcha verified");
+  //   }
 
-  let cognitoCheckUserResult = await signupHelpers.checkEmailExisted(
-    body.email
-  );
-  try {
-    if (cognitoCheckUserResult) {
-      //for security reasons we return 200 if email is found
-      return apiResponses._200({ message: "you can not use this email" });
+  //   let cognitoCheckUserResult = await signupHelpers.checkEmailExisted(
+  //     body.email
+  //   );
+
+  //   try {
+  //     console.log("here is the cognito check");
+  //     if (cognitoCheckUserResult) {
+  //       //for security reasons we return 200 if email is found
+  //       return apiResponses._200({ message: "you can not use this email" });
+  //     }
+  //   } catch (err) {
+  //     return apiResponses._400({ message: "This email is valid" });
+  //   }
+
+    try {
+      console.log("here is the dynamo insert");
+      const token = Math.floor(100000 + Math.random() * 900000);
+      var registrationItem = {
+       id:uuidv4(),
+       recipient: email,
+       token: token,
+       createdAt: Date.now(),
+       expiresAt: Math.floor(Date.now() / 1000) + 86400,
+       payload: {
+         email: email,
+         firstNaame: firstName,
+         lastName: lastName,
+         timestamp: Date.now(),
+         expiresAt: Math.floor(Date.now() / 1000) + 86400, //add 1 Day
+       }
+     };
+      const dynamoDbResult = await signupHelpers.upsertSignupUserToDynamoDb(registrationItem);
+      if (dynamoDbResult) {
+        return apiResponses._200({ message: "User added" });
+      }
+    } catch (err) {
+      return apiResponses._400({ message: "Cannot add user" });
     }
-  } catch (err) {
-    return err;
-  }
-
-  try {
-    var dynamoResult = await signupHelpers.checkIfUserExistsInDynamo(
-      body.email
-    );
-
-    if (!dynamoResult) {
-      let id = uuidv4();
-      let recipient = JSON.stringify(email);
-      let lastName = JSON.stringify(body.lastName);
-      let firstName = JSON.stringify(body.firstName);
-      let token = JSON.stringify(Math.floor(100000 + Math.random() * 900000));
-      let createdAt = Date.now();
-      let expiresAt = Math.floor(Date.now() / 1000) + 86400;
-
-      console.log(id , recipient , token , firstName , lastName)
-      const dynamoInsertResult = await signupHelpers.upsertSignupUserToDynamoDb(
-        id,
-        recipient,
-        token,
-        firstName,
-        lastName,
-        createdAt,
-        expiresAt
-      );
-      if (dynamoInsertResult) {
-        return apiResponses._200({ message: "User Inserted" });
-      } else return apiResponses._400({ message: "cannot insert user" });
-    }
-  } catch (err) {
-    console.log("Error:", err);
-    return apiResponses._400({ message: "error" });
-  }
+  // } catch (err) {
+  //   console.log("Error:", err);
+  //   return apiResponses._400({ message: "error" });
+  // }
 };
