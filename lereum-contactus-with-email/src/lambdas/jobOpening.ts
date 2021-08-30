aabc1cb959ba914fc5e009a83eb4ca6d606ff5ba
ghadiAtLeareum <85280321+ghadiAtLeareum@users.noreply.github.com>
2021-08-30 15:03:11 +0300

    refactoring

--- lereum-contactus-with-email/src/lambdas/jobOpening.ts
+++ lereum-contactus-with-email/src/lambdas/jobOpening.ts
@@ -9,7 +9,7 @@ import type {
   APIGatewayEvent,
   APIGatewayProxyResult,
 } from "aws-lambda";
-import { uploadCVToS3, getS3File, getPayload } from "../utils";
+import { uploadCVToS3, sendEmailWithAttach } from "../utils";
 
 const ses = new SES({ region: "us-east-1" });
 
@@ -25,64 +25,7 @@ export async function sendemailwithresume(
   try {
     const result = await uploadCVToS3(event, headers);
     if (result) {
-      const body = await getPayload(event);
-      const {
-        name,
-        from,
-        message,
-        nationality,
-        phoneNumber,
-        jobTitle,
-        jobLocation,
-      } = body;
-      console.log("This is the body");
-
-      const key = jobTitle + "/" + body.name + ".pdf";
-      console.log("this is key");
-      console.log(key);
-
-      const bucket = "lereum-jobopening-bucket2";
-
-      const attach = await getS3File(bucket, key);
-
-      console.log("this is the s3 body");
-      console.log(attach.Body);
-
-      const mailOptions = {
-        from: "auto@auto.lereum.com",
-        subject: "New Job Application!",
-        html: `<p>You got a contact message from: <b>${from}</b></p>
-                <p>My Name Is : <b>${name}</b></p>
-                <p>Applying As : <b>${jobTitle}</b></p>
-                <p><p>Job Location: <b>${jobLocation}</b></p>
-                <p>Nationality : <b>${nationality}</b></p>
-                <p>My Phone: <b>${phoneNumber}</b></p>
-                <p>Message: <b>${message}</b></p>`,
-        to: "ghadi@lereum.com",
-        bcc: ["hr@lereum.com"],
-        attachments: [
-          {
-            filename: body.name + ".pdf",
-            content: attach.Body.toString("base64"),
-            encoding: "base64",
-          },
-        ],
-      };
-      console.log(mailOptions);
-
-      const transporter = nodemailer.createTransport({
-        SES: ses,
-      });
-
-      // send email
-      transporter.sendMail(mailOptions, function (err, info) {
-        if (err) {
-          console.log(err);
-          console.log("Error sending email");
-        } else {
-          console.log("Email sent successfully");
-        }
-      });
+      await sendEmailWithAttach(event);
 
       return {
         statusCode: 200,
--- lereum-contactus-with-email/src/utils.ts
+++ lereum-contactus-with-email/src/utils.ts
@@ -3,7 +3,9 @@ import type { APIGatewayEvent } from "aws-lambda";
 import { jsonContentType } from "./schemas";
 import { SES } from "aws-sdk";
 import * as fileType from "file-type";
+import * as nodemailer from "nodemailer";
 
+const ses = new SES({ region: "us-east-1" });
 const s3 = new AWS.S3();
 
 export const getContentType = (event: APIGatewayEvent) => {
@@ -73,7 +75,6 @@ export const createSESSendEmailParamsForPartnerShip = (
   };
 };
 
-
 export const uploadCVToS3 = async (event: APIGatewayEvent, headers) => {
   const body = await getPayload(event);
   const { base64CV, jobTitle, name } = body;
@@ -112,5 +113,64 @@ export const uploadCVToS3 = async (event: APIGatewayEvent, headers) => {
 };
 
 export const getS3File = async (bucket, key) => {
-  return s3.getObject({ Bucket: bucket, Key: key }).promise()
+  return s3.getObject({ Bucket: bucket, Key: key }).promise();
+};
+
+export const sendEmailWithAttach = async (event: APIGatewayEvent) => {
+  const body = await getPayload(event);
+  const {
+    name,
+    from,
+    message,
+    nationality,
+    phoneNumber,
+    jobTitle,
+    jobLocation,
+  } = body;
+  console.log("This is the body");
+
+  const key = jobTitle + "/" + body.name + ".pdf";
+  console.log("this is key");
+  console.log(key);
+
+  const bucket = "lereum-jobopening-bucket2";
+
+  const attach = await getS3File(bucket, key);
+
+  console.log("this is the s3 body");
+  console.log(attach.Body);
+
+  const mailOptions = {
+    from: "auto@auto.lereum.com",
+    subject: "New Job Application!",
+    html: `<p>You got a contact message from: <b>${from}</b></p>
+                <p>My Name Is : <b>${name}</b></p>
+                <p>Applying As : <b>${jobTitle}</b></p>
+                <p><p>Job Location: <b>${jobLocation}</b></p>
+                <p>Nationality : <b>${nationality}</b></p>
+                <p>My Phone: <b>${phoneNumber}</b></p>
+                <p>Message: <b>${message}</b></p>`,
+    to: "ghadi@lereum.com",
+    attachments: [
+      {
+        filename: body.name + ".pdf",
+        content: attach.Body.toString("base64"),
+        encoding: "base64",
+      },
+    ],
+  };
+  console.log(mailOptions);
+
+  const transporter = nodemailer.createTransport({
+    SES: ses,
+  });
+
+  // send email
+  transporter.sendMail(mailOptions, function (err, info) {
+    if (err) {
+      return err;
+    } else {
+      return true;
+    }
+  });
 };
